9d9469db5f2befb9432c3a58cb1663c6f1b52f61
Nadezhda Durovich <effnylife@gmail.com>
2023-04-22 09:59:25 +0500

    The task is solved

--- 18_find the element closest in size to a given number.py
+++ 18_find the element closest in size to a given number.py
@@ -7,18 +7,19 @@
 #     1 2 3 4 5
 #     6
 #     -> 5
+from random import randint
 N = int(input('Enter the number (array size): '))
-array = [i for i in range(1, N+1)]
+array = [randint(1, 10) for i in range(1, N+1)]
 print(f"The array of {N} numbers is set: ", array, end=' ')
 print()
 X = int(input('Enter the number: '))
-min_value = array[0]
+closest_number = closest_number1 = closest_number2 = temp_number = array[0]
 for item in array:
     if item == X:
-        print(f"The number {item} is equal to the specified value {X}.")
+        print(f"The element {item} is equal to {X}")
+        closest_number = item
+        break
     else:
-        if abs(item-X) < abs(min_value-X):
-            min_value = array[item]
-            print(f"The array number closest in size to {X} is {min_value}")
-        # else:
-        #     print(f"The array number closest in size to {X} is {min_value}")
+        if abs(item-X) < abs(closest_number-X):
+            closest_number = item
+print(f"The array number closest in size to {X} is {closest_number}")
