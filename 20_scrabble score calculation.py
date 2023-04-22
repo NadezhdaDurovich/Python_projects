c19b0833a2f16570ab2f88bdfd1b355ff204755e
Nadezhda Durovich <effnylife@gmail.com>
2023-04-22 11:48:20 +0500

    The task is solved (formatted)

--- 20_scrabble score calculation.py
+++ 20_scrabble score calculation.py
@@ -11,11 +11,10 @@
 # 12
 
 
-scrabble_dict_eng = {1: 'A,E,I,O,U,L,N,S,T,R', 2: 'D,G',
-                     3: 'B,C,M,P', 4: 'F,H,V,W,Y', 5: 'K', 8: 'J, X', 10: 'Q,Z'}
-# scrabble_dict_rus={1:'А,В,Е,И,Н,О,Р,С,Т',2:'Д,К,Л,М,П,У',3:'Б,Г,Ё,Ь,Я',4:'Й,Ы',5:'Ж,З,Х,Ц,Ч',8:'Ш,Э,Ю',10:'Ф,Щ,Ъ'}
+scrabble_dict = {1: 'A,E,I,O,U,L,N,S,T,R', 2: 'D,G',
+                 3: 'B,C,M,P', 4: 'F,H,V,W,Y', 5: 'K', 8: 'J, X', 10: 'Q,Z', 1: 'А,В,Е,И,Н,О,Р,С,Т', 2: 'Д,К,Л,М,П,У', 3: 'Б,Г,Ё,Ь,Я', 4: 'Й,Ы', 5: 'Ж,З,Х,Ц,Ч', 8: 'Ш,Э,Ю', 10: 'Ф,Щ,Ъ'}
 word = input('Enter any word: ').upper()
 for i in word.split():
     print(
-        f"Your score points is {sum([k for i in word for k, v in scrabble_dict_eng.items() if i in v])}")
+        f"Your score points is {sum([k for i in word for k, v in scrabble_dict.items() if i in v])}")
     # print({sum([k for i in word for k, v in scrabble_dict_rus.items() if i in v])})
